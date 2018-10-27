f7080cad34405d70779a9ce18f8ddc8e3a5ecf16
Administrator <513125483@qq.com>
2018-10-27 17:16:18 +0800

    添加树形控件，浏览角色

--- src/components/system-manager/role.vue
+++ src/components/system-manager/role.vue
@@ -3,27 +3,7 @@
     :props="props"
     :load="loadNode"
     lazy
-    show-checkbox
-    node-key="id"
-    default-expand-all
-    :expand-on-click-node="false">
-    <span class="custom-tree-node" slot-scope="{ node, data }">
-        <span>{{ node.label }}</span>
-        <span>
-          <el-button
-            type="text"
-            size="mini"
-            @click="() => append(data)">
-            Append
-          </el-button>
-          <el-button
-            type="text"
-            size="mini"
-            @click="() => remove(node, data)">
-            Delete
-          </el-button>
-        </span>
-      </span>
+    show-checkbox>
   </el-tree>
 </template>
 
@@ -40,34 +20,18 @@
     data() {
       return {
         props: {
-          label: 'name',
+          label: 'remark',
           isLeaf: 'isLeaf'
         },
       };
     },
     methods: {
-      append(data) {
-        const newChild = {id: id++, label: 'testtest', children: []};
-        if (!data.children) {
-          this.$set(data, 'children', []);
-        }
-        data.children.push(newChild);
-      },
-
-      remove(node, data) {
-        const parent = node.parent;
-        const children = parent.data.children || parent.data;
-        const index = children.findIndex(d => d.id === data.id);
-        children.splice(index, 1);
-      },
-
       loadNode(node, resolve) {
         if (node.level === 0) {
           this._getRolesByParentId(1, resolve);
         }
-
-        if (node.level > 1) {
-          this._getRolesByParentId(node.level, resolve);
+        if (node.level >= 1) {
+          this._getRolesByParentId(node.data.roleId, resolve);
         }
       },
 
